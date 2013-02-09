a6b82db51ddf253546291f92fdab758eb9165b61
Konrad Malawski <konrad.malawski@project13.pl>
2013-02-09 19:31:27 +0100

    working on sonatype deploy

--- project/Build.scala
+++ project/Build.scala
@@ -15,7 +15,48 @@ object BuildSettings {
   )
 }
 
-object ScalaMacroDebugBuild extends Build {
+trait SonatypeSettings {
+  val sonatypeSettings = Seq(
+      publishMavenStyle := true,
+      publishArtifact in Test := false,
+      pomIncludeRepository := { _ => false },
+      publishTo <<= version { (v: String) =>
+        val nexus = "https://oss.sonatype.org/"
+        if (v.trim.endsWith("SNAPSHOT"))
+          Some("snapshots" at nexus + "content/repositories/snapshots")
+        else
+          Some("releases" at nexus + "service/local/staging/deploy/maven2")
+      },
+      credentials += Credentials(Path.userHome / ".sbt" / "sonatype.properties"),
+      pomExtra := (
+      <url>http://ktoso.github.com/scala-macro-delegate</url>
+      <licenses>
+        <license>
+          <name>Apache 2 License</name>
+          <url>http://www.apache.org/licenses/LICENSE-2.0.html</url>
+          <distribution>repo</distribution>
+        </license>
+      </licenses>
+      <scm>
+        <url>git@github.com:ktoso/scala-macro-delegate.git</url>
+        <connection>scm:git:git@github.com:ktoso/scala-macro-delegate.git</connection>
+      </scm>
+      <developers>
+        <developer>
+          <id>ktoso</id>
+          <name>Konrad 'ktoso' Malawski</name>
+          <url>http://blog.project13.pl</url>
+        </developer>
+      </developers>
+      <parent>
+        <groupId>org.sonatype.oss</groupId>
+        <artifactId>oss-parent</artifactId>
+        <version>7</version>
+      </parent>)
+      )
+}
+
+object ScalaMacroDebugBuild extends Build with SonatypeSettings {
   import BuildSettings._
 
   val scalaTest = "org.scalatest" %% "scalatest" % "2.0.M5b" % "test"
@@ -23,7 +64,7 @@ object ScalaMacroDebugBuild extends Build {
   lazy val root: Project = Project(
     "root",
     file("."),
-    settings = buildSettings
+    settings = buildSettings ++ sonatypeSettings
   ) aggregate(macros, examples)
 
   lazy val macros: Project = Project(
