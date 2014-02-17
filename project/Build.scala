c080541399d598fb3af50a94ae1133e0b35e8c3c
Konrad Malawski <konrad.malawski@project13.pl>
2014-02-17 19:06:07 +0000

    fixed build self-depending by name

--- project/Build.scala
+++ project/Build.scala
@@ -3,10 +3,8 @@ import Keys._
 
 object BuildSettings {
   val buildSettings = Defaults.defaultSettings ++ Seq (
-    organization  := "pl.project13.scala",
-    name          := "macro-method-alias",
     version       := "1.0",
-    scalaVersion  := "2.10.0"
+    scalaVersion  := "2.10.2"
   )
 }
 
@@ -54,26 +52,23 @@ trait SonatypeSettings {
 object ScalaMacroDebugBuild extends Build with SonatypeSettings {
   import BuildSettings._
 
-  val scalaTest = "org.scalatest" %% "scalatest" % "2.0.M5b" % "test"
+  val scalaTest = "org.scalatest" %% "scalatest" % "2.0" % "test"
 
-  lazy val root: Project = Project(
-    "root",
-    file("."),
-    settings = buildSettings
+  lazy val root: Project = Project("root", file("."),
+    settings = buildSettings ++ Seq(
+      organization  := "pl.project13.scala",
+      name          := "macro-method-alias"
+    )
   ) aggregate(macros, examples)
 
-  lazy val macros: Project = Project(
-    "macros",
-    file("macros"),
+  lazy val macros: Project = Project("macros", file("macros"),
     settings = buildSettings ++ sonatypeSettings ++ Seq(
       libraryDependencies <+= (scalaVersion)("org.scala-lang" % "scala-compiler" % _),
       libraryDependencies += scalaTest
     )
   )
 
-  lazy val examples: Project = Project(
-    "examples",
-    file("examples"),
+  lazy val examples: Project = Project("examples", file("examples"),
     settings = buildSettings ++ Seq(
       libraryDependencies += scalaTest
     )
